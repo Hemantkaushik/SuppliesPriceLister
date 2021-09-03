609260ddc35dd3554ab532125cfd41e9eb063fde
Hemant <Hemant@sportchamps.com.au>
2021-09-04 08:41:26 +1000

    Common exceptions for file operations

--- buildxact-supplies-price-lister-830fc692c659/Service/ProcessFileService.cs
+++ buildxact-supplies-price-lister-830fc692c659/Service/ProcessFileService.cs
@@ -30,6 +30,62 @@ namespace buildxact_supplies.Service
                 var dir = Directory.GetCurrentDirectory();
                 await _fileParserService.ProcessParsedFiles(dir + "\\humphries.csv", dir + "\\megacorp.json");
             }
+            catch (ArgumentNullException ex)
+            {
+                //   System.ArgumentNullException:
+                //     fileName is null.
+                _logger.LogError(ex.Message);
+            }
+            catch (System.Security.SecurityException ex)
+            {
+                //   System.Security.SecurityException:
+                //     The caller does not have the required permission.
+                _logger.LogError(ex.Message);
+            }
+            catch (ArgumentException ex)
+            {
+                //   System.ArgumentException:
+                //     The file name is empty, contains only white spaces, or contains invalid
+                //     characters.
+                _logger.LogError(ex.Message);
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                //   System.UnauthorizedAccessException:
+                //     Access to fileName is denied.
+                _logger.LogError(ex.Message);
+            }
+            catch (PathTooLongException ex)
+            {
+                //   System.IO.PathTooLongException:
+                //     The specified path, file name, or both exceed the system-defined maximum
+                //     length. For example, on Windows-based platforms, paths must be less than
+                //     248 characters, and file names must be less than 260 characters.
+                _logger.LogError(ex.Message);
+            }
+            catch (NotSupportedException ex)
+            {
+                //   System.NotSupportedException:
+                //     fileName contains a colon (:) in the middle of the string.
+                _logger.LogError(ex.Message);
+            }
+            catch (FileNotFoundException ex)
+            {
+                // System.FileNotFoundException
+                //  The exception that is thrown when an attempt to access a file that does not
+                //  exist on disk fails.
+                _logger.LogError(ex.Message);
+            }
+            catch (IOException ex)
+            {
+                //   System.IO.IOException:
+                //     An I/O error occurred while opening the file.
+                _logger.LogError(ex.Message);
+            }
+            catch (CsvHelper.HeaderValidationException ex)
+            {
+                _logger.LogError(ex.Message);
+            }
             catch (Exception ex)
             {
                 _logger.LogError("Processed stopped because of unhandled exception" + ex.Message);
